2961e1f20764f3070f393e30a18d00ab5ad65d6b
=AAGoitia701 <aagoitia701@gmail.com>
2025-01-28 21:18:49 +0100

    Se añadió evento click en menu, se creó menú para dispositivos, uso de mediaqueries

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.linesBurgerMenu').addEventListener('click', function () {
+    const menu = document.querySelector('.burgerMenu');
+    menu.classList.toggle('active'); // Alterna la clase "active"
+});
